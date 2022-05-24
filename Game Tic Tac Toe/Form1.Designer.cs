7c905b8bdbe47c27659af887dad5224f1b75bb7a
MJ852 <maryjoycecalangian@gmail.com>
2022-05-25 00:08:23 +0800

    Added modifcations and code

--- Game Tic Tac Toe/Form1.Designer.cs
+++ Game Tic Tac Toe/Form1.Designer.cs
@@ -44,8 +44,10 @@
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
             this.lblTitle.Font = new System.Drawing.Font("Agency FB", 49.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(190, 9);
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTitle.Location = new System.Drawing.Point(193, 21);
             this.lblTitle.Name = "lblTitle";
             this.lblTitle.Size = new System.Drawing.Size(364, 100);
             this.lblTitle.TabIndex = 0;
@@ -53,20 +55,26 @@
             // 
             // btn1
             // 
+            this.btn1.BackColor = System.Drawing.Color.Transparent;
+            this.btn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn1.Font = new System.Drawing.Font("Calibri", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.SystemColors.Window;
             this.btn1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.btn1.Location = new System.Drawing.Point(115, 201);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(168, 151);
             this.btn1.TabIndex = 1;
             this.btn1.Text = " ";
-            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.UseVisualStyleBackColor = false;
             this.btn1.Click += new System.EventHandler(this.button1_Click);
             // 
             // lblPlayer
             // 
             this.lblPlayer.AutoSize = true;
+            this.lblPlayer.BackColor = System.Drawing.Color.Transparent;
             this.lblPlayer.Font = new System.Drawing.Font("Agency FB", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.lblPlayer.Location = new System.Drawing.Point(51, 136);
             this.lblPlayer.Name = "lblPlayer";
             this.lblPlayer.Size = new System.Drawing.Size(105, 44);
@@ -76,96 +84,129 @@
             // 
             // btn2
             // 
+            this.btn2.BackColor = System.Drawing.Color.Transparent;
+            this.btn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn2.Font = new System.Drawing.Font("Calibri", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.SystemColors.Window;
             this.btn2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.btn2.Location = new System.Drawing.Point(289, 201);
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(168, 151);
             this.btn2.TabIndex = 3;
             this.btn2.Text = " ";
-            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.UseVisualStyleBackColor = false;
             // 
             // btn3
             // 
+            this.btn3.BackColor = System.Drawing.Color.Transparent;
+            this.btn3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn3.Font = new System.Drawing.Font("Calibri", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.ForeColor = System.Drawing.SystemColors.Window;
             this.btn3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.btn3.Location = new System.Drawing.Point(463, 201);
             this.btn3.Name = "btn3";
             this.btn3.Size = new System.Drawing.Size(168, 151);
             this.btn3.TabIndex = 4;
             this.btn3.Text = " ";
-            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.UseVisualStyleBackColor = false;
             // 
             // btn4
             // 
+            this.btn4.BackColor = System.Drawing.Color.Transparent;
+            this.btn4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn4.Font = new System.Drawing.Font("Calibri", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.ForeColor = System.Drawing.SystemColors.Window;
             this.btn4.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.btn4.Location = new System.Drawing.Point(115, 358);
             this.btn4.Name = "btn4";
             this.btn4.Size = new System.Drawing.Size(168, 151);
             this.btn4.TabIndex = 5;
             this.btn4.Text = " ";
-            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.UseVisualStyleBackColor = false;
             // 
             // btn5
             // 
+            this.btn5.BackColor = System.Drawing.Color.Transparent;
+            this.btn5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn5.Font = new System.Drawing.Font("Calibri", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.ForeColor = System.Drawing.SystemColors.Window;
             this.btn5.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.btn5.Location = new System.Drawing.Point(289, 358);
             this.btn5.Name = "btn5";
             this.btn5.Size = new System.Drawing.Size(168, 151);
             this.btn5.TabIndex = 6;
             this.btn5.Text = " ";
-            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.UseVisualStyleBackColor = false;
             // 
             // btn6
             // 
+            this.btn6.BackColor = System.Drawing.Color.Transparent;
+            this.btn6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn6.Font = new System.Drawing.Font("Calibri", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.ForeColor = System.Drawing.SystemColors.Window;
             this.btn6.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.btn6.Location = new System.Drawing.Point(463, 358);
             this.btn6.Name = "btn6";
             this.btn6.Size = new System.Drawing.Size(168, 151);
             this.btn6.TabIndex = 7;
             this.btn6.Text = " ";
-            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.UseVisualStyleBackColor = false;
             // 
             // btn7
             // 
+            this.btn7.BackColor = System.Drawing.Color.Transparent;
+            this.btn7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn7.Font = new System.Drawing.Font("Calibri", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.ForeColor = System.Drawing.SystemColors.Window;
             this.btn7.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.btn7.Location = new System.Drawing.Point(115, 515);
             this.btn7.Name = "btn7";
             this.btn7.Size = new System.Drawing.Size(168, 151);
             this.btn7.TabIndex = 8;
             this.btn7.Text = " ";
-            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.UseVisualStyleBackColor = false;
             // 
             // btn8
             // 
+            this.btn8.BackColor = System.Drawing.Color.Transparent;
+            this.btn8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn8.Font = new System.Drawing.Font("Calibri", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.ForeColor = System.Drawing.SystemColors.Window;
             this.btn8.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.btn8.Location = new System.Drawing.Point(289, 515);
             this.btn8.Name = "btn8";
             this.btn8.Size = new System.Drawing.Size(168, 151);
             this.btn8.TabIndex = 9;
             this.btn8.Text = " ";
-            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.UseVisualStyleBackColor = false;
             // 
             // btn9
             // 
+            this.btn9.BackColor = System.Drawing.Color.Transparent;
+            this.btn9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn9.Font = new System.Drawing.Font("Calibri", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.ForeColor = System.Drawing.SystemColors.Window;
             this.btn9.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.btn9.Location = new System.Drawing.Point(463, 515);
             this.btn9.Name = "btn9";
             this.btn9.Size = new System.Drawing.Size(168, 151);
             this.btn9.TabIndex = 10;
             this.btn9.Text = " ";
-            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Game_Tic_Tac_Toe.Properties.Resources.blacxk;
             this.ClientSize = new System.Drawing.Size(771, 730);
             this.Controls.Add(this.btn9);
             this.Controls.Add(this.btn8);
